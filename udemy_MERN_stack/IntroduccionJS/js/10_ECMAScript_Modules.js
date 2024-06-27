e7a2942701b242e46cbcc77865db19aefcd38ee5
flipeprez <flipeprez@gmail.com>
2024-06-27 13:00:15 -0300

    modify 10 ecmas and function files

--- udemy_MERN_stack/IntroduccionJS/js/10_ECMAScript_Modules.js
+++ udemy_MERN_stack/IntroduccionJS/js/10_ECMAScript_Modules.js
@@ -12,3 +12,13 @@ const sus_result = restar( 15, 98 );
 
 console.log(`el resultado de la suma es: ${add_result} el resultado de la resta es: ${sus_result}`);
 
+
+//podemos realizar exportaciones usando alias en caso de tener funciones con el mismo nombre
+//we can use an alias in case if we have functions with the same name
+
+import { sumar as add_func, restar as sus_func } from './functions_test.js'; 
+
+const add_result1 = add_func( 20, 20 );
+const sus_result1 = sus_func( 15, 100 ); 
+
+console.log(`el resultado de la suma es: ${add_result1} el resultado de la resta es: ${sus_result1}`);
--- udemy_MERN_stack/IntroduccionJS/js/functions_test.js
+++ udemy_MERN_stack/IntroduccionJS/js/functions_test.js
@@ -1,7 +1,23 @@
-export function sumar(n1 , n2){
+/*export function sumar(n1 , n2){
     return (n1 + n2)
 } 
 
 export function restar(n1 , n2){
     return (n1 - n2)
+}
+*/
+//otra forma de exportar 
+//other way to make the export 
+
+function sumar(n1 , n2){
+    return (n1 + n2)
+} 
+
+function restar(n1 , n2){
+    return (n1 - n2)
+}
+
+export {
+    sumar,
+    restar
 }

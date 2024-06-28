bdd5f8f9691c5fb3c2c20cef0d7b20bbdb0f9e79
flipeprez <flipeprez@gmail.com>
2024-06-28 17:23:49 -0300

    is not workinggit add 14_validations_\&_alerts.js

--- udemy_MERN_stack/IntroduccionJS/js/14_validations_&_alerts.js
+++ udemy_MERN_stack/IntroduccionJS/js/14_validations_&_alerts.js
@@ -13,12 +13,12 @@ if (preWarning){
 }
 
 const warning = document.createElement('DIV')
-alerta.classList.add('alerta', 'text-white', 'uppercase', 'text-sm', 'p-2', 'font-black')
+warning.classList.add('alerta', 'text-white', 'uppercase', 'text-sm', 'p-2', 'font-black')
 
-if (user === '' || password === '') {
-    alerta.textContent = 'complete all the fields'
-    alerta.classList.add('bg-red-500')
+if (nombre === '' || password === '') {
+    warning.textContent = 'complete all the fields'
+    warning.classList.add('bg-red-500')
 } else {
-    alerta.textContent = 'loading'
-    alerta.classList.add('bg-green-500')
+    warning.textContent = 'loading'
+    warning.classList.add('bg-green-500')
 }
